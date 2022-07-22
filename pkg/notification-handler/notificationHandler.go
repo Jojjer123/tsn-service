2e7d4b16a1f576f8dfee0ff585f7b245f9a0cdb6
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-07-22 07:35:30 +0200

    Remove unushed old commented code.

--- pkg/notification-handler/notificationHandler.go
+++ pkg/notification-handler/notificationHandler.go
@@ -42,27 +42,13 @@ func CalculateConfiguration(ids []string) (string, error) {
 		return "", err
 	}
 
-	// TODO: REDO EVERYTHING BELOW THIS COMMENT, CONFIGURATION IS NOT RESPONSE FILE
-
-	// TODO: Have template of configuration (not response file, response file should only be in UNI or handler of main-service)
-	// config, err := genConfigTemplate(allRequestData)
-	// // _, err = generateBaseResponse(reqData)
-	// if err != nil {
-	// 	log.Errorf("Failed generating response template: %v", err)
-	// 	return "", nil
-	// }
-
-	// log.Info(resp)
+	// TODO: Have template of configuration
 
 	// TODO: Load template with random values (simulate applying configuration)
 
 	confId := fmt.Sprintf("%v", uuid.New())
 
 	// TODO: Store configuration in k/v store
-	// if err = store.StoreConfiguration(config, confId); err != nil {
-	// 	log.Errorf("Failed storing configurations: %v", err)
-	// 	return "", nil
-	// }
 
 	return confId, nil
 }
